441d32b66b5bff3211421f908dd53139f195b83d
elenakikiova <elenakikiova@mail.ru>
2019-12-30 15:01:27 +0200

    Workout manager

--- dotsIonic/src/app/app-routing.module.ts
+++ dotsIonic/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { WorkoutsPage } from './workouts/workouts.page'
 import { WorkoutSheetPage } from './workoutSheet/workoutSheet.page'
+import { WorkoutManagerPage } from './workoutManager/workoutManager.page'
 
 const routes: Routes = [
   {
@@ -25,6 +26,10 @@ const routes: Routes = [
   {
     path: 'workoutSheet/:sheetId',
     loadChildren: () => import('./workoutSheet/workoutSheet.module').then(m => m.WorkoutSheetPageModule)
+  },
+  {
+    path: 'workoutManager/:sheetId',
+    loadChildren: () => import('./workoutManager/workoutManager.module').then(m => m.WorkoutManagerPageModule)
   }
 ];
 
---
+++ dotsIonic/src/app/workoutManager/workoutManager.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { WorkoutManagerPage } from './workoutManager.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: WorkoutManagerPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NgxChartsModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [WorkoutManagerPage]
+})
+export class WorkoutManagerPageModule {}
