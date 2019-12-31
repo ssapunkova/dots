e3e567f8a207e3e3790bec2ea18584a8ae3e5c59
elenakikiova <elenakikiova@mail.ru>
2019-12-31 13:56:04 +0200

    Comments in timer service

--- dotsIonic/src/app/services/timer.service.ts
+++ dotsIonic/src/app/services/timer.service.ts
@@ -4,8 +4,8 @@ import { TimeAndDateService } from './timeAndDate.service';
 
 import { interval } from 'rxjs';
 
-// Chart Service
-// Implements formatting and displaying record data in a line chart
+// Timer Service
+// Implements setting timers and countdowns
 
 @Injectable()
 export class TimerService{
@@ -15,46 +15,51 @@ export class TimerService{
 
   public timer;
 
+  // Sets timer
+  async setTimer(){
+    this.startDate = new Date();
+    this.currentDate = new Date();
+    this.playTimer();
+  }
+
+  // Increments seconds
   async timerFunc(){
     console.log(this.currentDate);
     this.currentDate++;
 
   }
 
-  async setTimer(){
-    this.startDate = new Date();
-    this.currentDate = new Date();
-    this.timer = interval(1000).subscribe(x => {
-      this.timerFunc()
-    });
-  }
-
+  // Pauses timer
   async pauseTimer(){
     this.timer.unsubscribe();
   }
 
+  // Start/restart the timer
   async playTimer(){
     this.timer = interval(1000).subscribe(x => {
       this.timerFunc()
     });
   }
 
+  // Set a countdown in seconds
+  // seconds - how many seconds will the countdown last
+  // whileFunc - function that is be called every second
+  // afterFunc - function that is called after the countdown ends
   async setCountdown(seconds, whileFunc, afterFunc){
     let secondsLeft = seconds;
     this.timer = interval(1000).subscribe(x => {
       if(secondsLeft > 0){
-        // Decrement secondsLeft and execute some actions while the countdown is running
         secondsLeft--;
         whileFunc(secondsLeft);
       }
       else{
-        // Execute actions after the timer has stopped
         this.timer.unsubscribe();
         afterFunc();
       }
     });
   }
 
+  // Stop the countdown
   async stopCountdown(){
     this.timer.unsubscribe();
   }
