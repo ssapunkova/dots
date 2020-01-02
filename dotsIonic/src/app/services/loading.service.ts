176235db62d8a920cf81570152213f5a2e9e18a3
elenakikiova <elenakikiova@mail.ru>
2020-01-02 12:57:19 +0200

    Comments in loadingService

--- dotsIonic/src/app/services/loading.service.ts
+++ dotsIonic/src/app/services/loading.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 
+// Loading Service
+// Implements showing and hiding loadings
+
 @Injectable()
 export class LoadingService{
 
-  public isPageLoading = true;
-  public isProcessLoading = false;
+  public isPageLoading = true;                // Controlls full-screen loader
+  public isProcessLoading = false;            // Controlls small loading
   public processLoader;
 
   constructor(
       public loadingController: LoadingController,
   ) { }
 
+  // Show small loading alert with custom message
   async presentSmallLoading(message){
     this.isProcessLoading = true;
     this.processLoader = await this.loadingController.create({
@@ -20,6 +24,7 @@ export class LoadingService{
     await this.processLoader.present();
   }
 
+  // Hide small loading
   async dismissSmallLoading(){
     if(this.isProcessLoading) this.processLoader.dismiss();
   }
