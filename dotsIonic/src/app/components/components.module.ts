ce37102e2b07c2a059b567b9ba7360bdf12a3ce4
elenakikiova <elenakikiova@mail.ru>
2020-01-22 21:26:10 +0200

    ion-select for showing period has returned!

--- dotsIonic/src/app/components/components.module.ts
+++ dotsIonic/src/app/components/components.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { FormsModule } from '@angular/forms';
+
 import { ChartTooltipComponent } from './chart-tooltip/chart-tooltip.component';
 import { ChartLegendComponent } from './chart-legend/chart-legend.component';
 import { DataTableComponent } from './data-table/data-table.component';
@@ -19,7 +21,8 @@ import { ToggleShowingMode } from './toggle-showing-mode/toggle-showing-mode.com
   imports: [
     CommonModule,
     IonicModule,
-    NgxChartsModule
+    NgxChartsModule,
+    FormsModule
   ],
   exports: [
     ChartTooltipComponent,
