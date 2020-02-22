ab76f75956b559d578f075406ddd0d01a4bc2d68
Elena Kikiova <elenakikiova@mail.ru>
2020-02-22 10:03:07 +0200

    Select type of params in calculator

--- dotsIonic/src/app/calculate/calculate.page.ts
+++ dotsIonic/src/app/calculate/calculate.page.ts
@@ -10,46 +10,55 @@ import { ParamsService } from '../services/params.service';
 })
 export class CalculatePage implements OnInit {
 
+  // Converting measures
+
+  public convertToInches(cm){
+    return parseInt(parseInt(cm) / 2.54);
+  }
+  public convertToLb(kg){
+    return parseInt(parseInt(kg) * 2.204);
+  }
+
   public params = [
     {
       Title: "age",
-      Unit: "y.o.",
+      Unit: "y.o.",                                   // 0 - age
       Type: "number"
     },
     {
       Title: "gender",
       Options: [
         { "Title": "male", "Value": "m" },
-        { "Title": "female", "Value": "f" }
+        { "Title": "female", "Value": "f" }           // 1 - gender
       ],
       Unit: "",
       Type: "checkbox"
     },
     {
       Title: "weight",
-      Unit: "kg",
+      Unit: "kg",                                     // 2 - weight
       Type: "number"
     },
     {
       Title: "height",
-      Unit: "cm",
+      Unit: "cm",                                     // 3 - height
       Type: "number"
     },
     {
       Title: "waist",
-      Unit: "cm",
+      Unit: "cm",                                     // 4 - waist
       Type: "number"
     },
     {
       Title: "hips",
-      Unit: "cm",
+      Unit: "cm",                                     // 5 - hips
       Type: "number"
     },
     {
       Title: "activity factor",
       Options: [
         { "Title": "Seditary", "Value": 0.7 },
-        { "Title": "Light", "Value": 1 },
+        { "Title": "Light", "Value": 1 },             // 6 - activity factor
         { "Title": "Moderate", "Value": 1.3 }
       ],
       Type: "select"
@@ -59,7 +68,6 @@ export class CalculatePage implements OnInit {
   public formulas = {
     "Blocks": {
       required: [
-        this.params[0],
         this.params[1],
         this.params[2],
         this.params[3],
@@ -68,10 +76,11 @@ export class CalculatePage implements OnInit {
         this.params[6]
       ],
       formula: (values) => {
-        let age = values[0];
-        let gender = values[1];
-        if(age < 17) return "a";
-        else return "b";
+        // let gender = values[0];
+        let weight = this.convertToLb(values[1]);
+        let physicalActivity = values[6];
+        console.log(weight, physicalActivity);
+        return weight;
       }
     }
   }
