bf2fb09eca216f78cefaddf843dec98e39e8618d
elenakikiova <elenakikiova@mail.ru>
2020-03-19 10:51:27 +0200

    Update gitignore

--- dotsIonic/src/app/services/connectToServer.service.ts
+++ dotsIonic/src/app/services/connectToServer.service.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConnectToServerService{
 
-    serverUrl: string = "http://localhost:8080";
-    // serverUrl: string = "https://dots-node.now.sh";
-    // serverUrl: string = "https://dots-node.herokuapp.com";
+    // serverUrl: string = "http://localhost:8080";
+    serverUrl: string = "https://dots-node.herokuapp.com";
 
 }
