763de775985335436e8a72eb47dbdd70d5952a85
elenakikiova <elenakikiova@mail.ru>
2020-04-10 18:39:39 +0300

    Legend translation

--- dotsIonic/src/app/services/params.service.ts
+++ dotsIonic/src/app/services/params.service.ts
@@ -11,7 +11,7 @@ export class ParamsService {
   public allParams = [
     {
       Index: 0,
-      Title: "gender",
+      Title: "Gender",
       Options: [
         { "Title": "Male", "Value": 0 },
         { "Title": "Female", "Value": 1 },                 // 0 - gender  
@@ -20,43 +20,43 @@ export class ParamsService {
     },
     {
       Index: 1,
-      Title: "age",
+      Title: "Age",
       Unit: "y.o.",                                        // 1 - age
       Type: "number"
     },
     {
       Index: 2,
-      Title: "weight",
+      Title: "Weight",
       Unit: "kg",                                          // 2 - weight
       Type: "number"
     },
     {
       Index: 3,
-      Title: "height",
+      Title: "Height",
       Unit: "cm",                                          // 3 - height
       Type: "number"
     },
     {
       Index: 4,
-      Title: "waist",
+      Title: "Waist",
       Unit: "cm",                                          // 4 - waist
       Type: "number"
     },
     {
       Index: 5,
-      Title: "wrist",
+      Title: "Wrist",
       Unit: "cm",                                          // 5 - waist
       Type: "number"
     },
     {
       Index: 6,
-      Title: "hips",
+      Title: "Hips",
       Unit: "cm",                                          // 6 - hips
       Type: "number"
     },
     {
       Index: 7,
-      Title: "activityFactorZone",
+      Title: "ActivityFactorZone",
       Options: [
         { "Title": "seditary", "Value": 0.5 },
         { "Title": "light", "Value": 0.6 },    
@@ -69,7 +69,7 @@ export class ParamsService {
     },
     {
       Index: 8,
-      Title: "activityFactorKcal",
+      Title: "ActivityFactorKcal",
       Options: [
         { "Title": "low", "Value": 1.2 },
         { "Title": "average", "Value": 1.3 },              // 8 - activity factor for kcal calculations
@@ -86,28 +86,28 @@ export class ParamsService {
     },
     {
       Index: 10,
-      Title: "bodyFatPercentage",                          
+      Title: "BodyFatPercentage",                          
       Goal: 20,                                            // 10 - body fat percentage
       Unit: "%",
       Type: "number"
     },
     {
       Index: 11,
-      Title: "daylyProteinIntake",
+      Title: "DaylyProteinIntake",
       Goal: 85,                                            // 11 - dayly protein intake in gr
       Unit: "g",
       Type: "number"
     },
     {
       Index: 12,
-      Title: "blocks",
+      Title: "Blocks",
       Goal: 12,                                            // 12 - zone blocks
       Unit: "blocks",
       Type: "number"
     },
     {
       Index: 13,
-      Title: "sugar",
+      Title: "Sugar",
       Goal: 40,                                            // 13 - dayly sugar intake in gr
       Unit: "gr",
       Type: "number"
